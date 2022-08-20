7382856127115e9cdc66a39c62c98b7eabd0f61b
luka dodelia <luka@picktek.com>
2022-08-20 13:58:48 +0400

    admin endpoint

--- first_run.sh
+++ first_run.sh
@@ -15,6 +15,11 @@ curl -i -X POST \
 
 curl -i -X POST \
   --url http://localhost:8001/services/ \
+  --data 'name=ms-sample-admin-web-app' \
+  --data 'url=http://ms_sample_web_app:7702'
+
+curl -i -X POST \
+  --url http://localhost:8001/services/ \
   --data 'name=ms-sample-user' \
   --data 'url=http://ms_sample_user:7702/external'
 
@@ -33,6 +38,11 @@ curl -i -X POST \
   --url http://localhost:8001/services/ms-sample-web-app/routes \
   --data 'name=ms-sample-web-app' \
   --data 'hosts[]=app.example.com'
+
+curl -i -X POST \
+  --url http://localhost:8001/services/ms-sample-admin-web-app/routes \
+  --data 'name=ms-sample-admin-web-app' \
+  --data 'hosts[]=admin.example.com'
 
 curl -i -X POST \
   --url http://localhost:8001/services/ms-sample-user/routes \
